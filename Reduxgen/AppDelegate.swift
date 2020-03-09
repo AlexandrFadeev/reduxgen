22b2828039771962533a6b126ccd9fbda99d726e
Alexandr Fadeev <alexandr@bend.md>
2020-03-09 17:05:02 +0200

    initial commit

---
+++ Reduxgen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Reduxgen
+//
+//  Created by Alex on 3/9/20.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Reduxgen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reduxgen
+//
+//  Created by Alex on 3/9/20.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
+
